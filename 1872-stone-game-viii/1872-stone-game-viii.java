42a63d517809e0437df545094deb534501db696b
Rohit Garudkar <rohitgarudakar@gmail.com>
2026-08-24 13:39:52 +0530

    Time: 4 ms (66.81%), Space: 92.1 MB (37.02%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -4,16 +4,12 @@ class Solution {
 
         int[] prefix = stones.clone();
 
-        // Build prefix sums
         for (int i = 1; i < n; i++) {
             prefix[i] += prefix[i - 1];
         }
-
-        // If Alice takes all stones,
-        // the game ends immediately.
+        
         int best = prefix[n - 1];
-
-        // Try every earlier valid prefix
+        
         for (int i = n - 2; i >= 1; i--) {
             best = Math.max(best, prefix[i] - best);
         }
